2992b3000b5e48f7ec1f8f3e824e52fab81fe4e8
rockrider001 <gllier@163.com>
2021-01-27 12:01:30 +0800

    Add nofity frame

---
+++ notify/inc/notify.h
@@ -0,0 +1,25 @@
+/**
+ * @file nodify.h
+ * @brief
+ * @author rock (gllier@163.com)
+ * @version 1.0
+ * @date 2021-01-27
+ * @copyright Copyright (c) 2020 - 2021 Nanjing GWDR Power Technology Co., Ltd.
+ * @par History:
+ * <table>
+ * <tr><th>Date       <th>Version <th>Author  <th>Description
+ * <tr><td>2021-01-27 <td>1.0     <td>rock     <td>Create
+ * </table>
+ */
+#ifndef __NOTIFY_H__
+#define __NOTIFY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ notify/src/notify.c
@@ -0,0 +1,19 @@
+/**
+ * @file notify.c
+ * @brief
+ * @author rock (gllier@163.com)
+ * @version 1.0
+ * @date 2021-01-27
+ * @copyright Copyright (c) 2020 - 2021 Nanjing GWDR Power Technology Co., Ltd.
+ * @par History:
+ * <table>
+ * <tr><th>Date       <th>Version <th>Author  <th>Description
+ * <tr><td>2021-01-27 <td>1.0     <td>rock     <td>Create
+ * </table>
+ */
+#include "notify.h"
+
+int notify_init(void)
+{
+	return 0;
+}
