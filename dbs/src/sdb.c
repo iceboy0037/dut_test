4c64a5cf6a96b0ed2e7c8a1ebd1e48cb331a9e1e
rockrider001 <gllier@163.com>
2021-03-02 21:27:40 -0800

    update param

--- dbs/src/sdb.c
+++ dbs/src/sdb.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sqlite3.h>
 
 #include "dbs.h"
---
+++ tools/calibrate/calibrate.h
@@ -0,0 +1,16 @@
+#ifndef __CALIBRATE__
+#define __CALIBRATE__
+
+#define CHANNEL_NUMBER		80
+#define CALIBRATE_PHASE		4
+
+
+struct coef_item_t {
+	float coef;
+	int low;
+	int high;
+};
+
+
+
+#endif
